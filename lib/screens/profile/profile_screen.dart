69a0e9e8ed3e1533630fa6470880c6dd11cea4e7
Ahmad Abdullah <abdullahhrajput4943@gmail.com>
2026-04-29 19:20:56 +0500

    Updateprofile screen

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,15 +1,15 @@
-// lib/screens/profile/profile_screen.dart
-import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'mobile/mobile_profile.dart';
-import 'web/web_profile.dart';
+// // lib/screens/profile/profile_screen.dart
+// import 'package:flutter/material.dart';
+// import 'package:flutter/foundation.dart' show kIsWeb;
+// import 'mobile/mobile_profile.dart';
+// import 'web/web_profile.dart';
 
-class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+// class ProfileScreen extends StatelessWidget {
+//   const ProfileScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    if (kIsWeb) return const WebProfile();
-    return const MobileProfile();
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     if (kIsWeb) return const WebProfile();
+//     return const MobileProfile();
+//   }
+// }
